3fa545fc43036eec9e38c603a8248006048a55ca
yaoqijun <yaoqijun@changingedu.com>
2017-09-04 15:09:44 +0800

    add init

---
+++ src/main/java/org/yqj/metric/demo/BootDemoApplication.java
@@ -0,0 +1,32 @@
+package org.yqj.metric.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-04-27
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+@SpringBootApplication
+@Controller
+public class BootDemoApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .banner(new DemoBanner())
+                .sources(BootDemoApplication.class)
+                .run(args);
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String indexPage(){
+        return "this is test index paging info";
+    }
+}
