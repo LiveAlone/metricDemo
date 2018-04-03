a87dd49e70841971b0636f1c117fd06b96cbd9a5
yaoqijun <yaoqijun@changingedu.com>
2018-04-03 13:14:58 +0800

    guage controller

---
+++ src/main/java/org/yqj/metric/demo/GaugeController.java
@@ -0,0 +1,26 @@
+package org.yqj.metric.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.yqj.metric.demo.springintegration.GaugeService;
+
+/**
+ * @author yaoqijun on 2018-04-03.
+ */
+@Controller
+public class GaugeController {
+
+    @Autowired
+    private GaugeService gaugeService;
+
+    @RequestMapping(value = "/gauge_add", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public void gaugeAddCount(){
+        gaugeService.addCount();
+    }
+
+}
---
+++ src/main/java/org/yqj/metric/demo/springintegration/GaugeService.java
@@ -0,0 +1,26 @@
+package org.yqj.metric.demo.springintegration;
+
+import com.codahale.metrics.annotation.Gauge;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author yaoqijun on 2018-04-03.
+ */
+@Service
+@Slf4j
+public class GaugeService {
+
+    private volatile int count = 0;
+
+    public void addCount(){
+        log.info("gauge count adder");
+        count = count + 1;
+    }
+
+    @Gauge(name = "method.name.gauge.count", absolute = true)
+    public int getCount(){
+        return count;
+    }
+
+}
